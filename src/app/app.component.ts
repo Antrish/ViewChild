3be6a8ea726e662a56634ac35962b39c087647c9
Antrish Mishra <Antrish@users.noreply.github.com>
2023-03-22 23:10:17 +0530

    project 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ChildComponentComponent } from './child-component/child-component.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ViewChild';
+  @ViewChild ('calendarDateInput') date!: ElementRef;
+  @ViewChild('vikramSamvatInput') vsDate!: ElementRef;
+  @ViewChild (ChildComponentComponent, {static:true}) childCompProperty!:ChildComponentComponent;
+
+  calculateVSYear(){ 
+    let currentYear = new Date(this.date.nativeElement.value).getFullYear();
+   let  vikramSamvatYear = currentYear +57;
+    this.vsDate.nativeElement.value = vikramSamvatYear;
+  }
 }
